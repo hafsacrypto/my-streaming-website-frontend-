2ebda1eaac6c24981ad7a1eaaa6065de3a8d7420
hafsacrypto <113556729+hafsacrypto@users.noreply.github.com>
2024-05-20 17:53:45 +0100

    updated the mainpage

--- src/Moviespage/MoviesCard.js
+++ src/Moviespage/MoviesCard.js
@@ -3,8 +3,8 @@ import React from 'react';
 function MoviesCard({ id, shows }) {
   console.log(shows[id].url)  
   return (
-    <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0, paddingRight: "15px" }}>
-      <iframe width="200" height="150" src={shows[id].url} title={shows[id].title} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+    <div style={{ position: 'relative', paddingBottom: '15%', height: 0, paddingRight: "15px" }}>
+      <iframe width="300rem" height="200rem" src={shows[id].url} title={shows[id].title} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
 </div>
   );
 }
